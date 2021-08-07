e0342bedd120b7875275c56da848010c6e109455
Andrii Yukhymenko <andriy111224@gmail.com>
2021-08-07 17:08:33 +0300

    Сделал первый и второй блоки + адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,7 @@ var burgerElement = document.querySelector(".menu__burger"),
     menuBody = document.querySelector(".menu__body"),
     menuList = menuBody.querySelector(".menu__list"),
     menuItems = menuBody.querySelectorAll(".menu__item"),
+    body = document.querySelector("body"),
     siteHeader = document.querySelector(".header");
 burgerElement.addEventListener("click", function () {
   menuBody.classList.toggle("active");
@@ -12,6 +13,7 @@ burgerElement.addEventListener("click", function () {
   menuItems.forEach(function (item) {
     item.classList.toggle("column");
   });
+  body.classList.toggle("not-scrollable");
 }); // window.addEventListener("scroll", () => {
 //   siteHeader.style =
 //     "position: fixed;" + "  top: 0;" + "  left: 0;" + "  right: 0;";
--- src/js/main.js
+++ src/js/main.js
@@ -2,16 +2,17 @@ const burgerElement = document.querySelector(".menu__burger"),
   menuBody = document.querySelector(".menu__body"),
   menuList = menuBody.querySelector(".menu__list"),
   menuItems = menuBody.querySelectorAll(".menu__item"),
+  body = document.querySelector("body"),
   siteHeader = document.querySelector(".header");
 
 burgerElement.addEventListener("click", () => {
   menuBody.classList.toggle("active");
   menuList.classList.toggle("active");
   burgerElement.classList.toggle("active");
-
   menuItems.forEach((item) => {
     item.classList.toggle("column");
   });
+  body.classList.toggle("not-scrollable");
 });
 
 // window.addEventListener("scroll", () => {
