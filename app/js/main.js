475dbdcf324a2c093f2024930efa6c158d59ac30
Andrii Yukhymenko <andriy111224@gmail.com>
2021-08-08 18:36:35 +0300

    Сделал блоки и слайдеры портфолио и отзывы

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,72 @@
 "use strict";
 
-var burgerElement = document.querySelector(".menu__burger"),
-    menuBody = document.querySelector(".menu__body"),
-    menuList = menuBody.querySelector(".menu__list"),
-    menuItems = menuBody.querySelectorAll(".menu__item"),
-    body = document.querySelector("body"),
-    siteHeader = document.querySelector(".header");
-burgerElement.addEventListener("click", function () {
-  menuBody.classList.toggle("active");
-  menuList.classList.toggle("active");
-  burgerElement.classList.toggle("active");
+var burgerElement = document.querySelector('.menu__burger'),
+    menuBody = document.querySelector('.menu__body'),
+    menuList = menuBody.querySelector('.menu__list'),
+    menuItems = menuBody.querySelectorAll('.menu__item'),
+    body = document.querySelector('body'),
+    siteHeader = document.querySelector('.header');
+burgerElement.addEventListener('click', function () {
+  menuBody.classList.toggle('active');
+  menuList.classList.toggle('active');
+  burgerElement.classList.toggle('active');
   menuItems.forEach(function (item) {
-    item.classList.toggle("column");
+    item.classList.toggle('column');
   });
-  body.classList.toggle("not-scrollable");
+  body.classList.toggle('not-scrollable');
 });
 
 function ibg() {
-  var ibg = document.querySelectorAll(".ibg");
+  var ibg = document.querySelectorAll('.ibg');
 
   for (var i = 0; i < ibg.length; i++) {
-    if (ibg[i].querySelector("img")) {
-      ibg[i].style.backgroundImage = "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+    if (ibg[i].querySelector('img')) {
+      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
     }
   }
 }
 
-ibg(); // Слайдер
-// window.addEventListener("scroll", () => {
+ibg(); // Слайдеры
+
+var miniPortfolioSlider = new Swiper('.mini-portfolio__slider', {
+  loop: true,
+  slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+    }
+  },
+  navigation: {
+    nextEl: '.mini-portfolio__controls-button--right',
+    prevEl: '.mini-portfolio__controls-button--left'
+  }
+});
+var reviewsImgSlider = new Swiper('.reviews-img-slider', {
+  loop: true,
+  slidesPerView: 1,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  navigation: {
+    nextEl: '.reviews__controls-button--right',
+    prevEl: '.reviews__controls-button--left'
+  }
+});
+var reviewsTextSlider = new Swiper('.reviews-text-slider', {
+  loop: true,
+  slidesPerView: 1,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  navigation: {
+    nextEl: '.reviews__controls-button--right',
+    prevEl: '.reviews__controls-button--left'
+  }
+});
+reviewsImgSlider.controller.control = reviewsTextSlider;
+reviewsTextSlider.controller.control = reviewsImgSlider; // window.addEventListener("scroll", () => {
 //   siteHeader.style =
 //     "position: fixed;" + "  top: 0;" + "  left: 0;" + "  right: 0;";
 // });
--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,78 @@
-const burgerElement = document.querySelector(".menu__burger"),
-  menuBody = document.querySelector(".menu__body"),
-  menuList = menuBody.querySelector(".menu__list"),
-  menuItems = menuBody.querySelectorAll(".menu__item"),
-  body = document.querySelector("body"),
-  siteHeader = document.querySelector(".header");
+const burgerElement = document.querySelector('.menu__burger'),
+  menuBody = document.querySelector('.menu__body'),
+  menuList = menuBody.querySelector('.menu__list'),
+  menuItems = menuBody.querySelectorAll('.menu__item'),
+  body = document.querySelector('body'),
+  siteHeader = document.querySelector('.header');
 
-burgerElement.addEventListener("click", () => {
-  menuBody.classList.toggle("active");
-  menuList.classList.toggle("active");
-  burgerElement.classList.toggle("active");
+burgerElement.addEventListener('click', () => {
+  menuBody.classList.toggle('active');
+  menuList.classList.toggle('active');
+  burgerElement.classList.toggle('active');
   menuItems.forEach((item) => {
-    item.classList.toggle("column");
+    item.classList.toggle('column');
   });
-  body.classList.toggle("not-scrollable");
+  body.classList.toggle('not-scrollable');
 });
 
 function ibg() {
-  let ibg = document.querySelectorAll(".ibg");
+  let ibg = document.querySelectorAll('.ibg');
   for (var i = 0; i < ibg.length; i++) {
-    if (ibg[i].querySelector("img")) {
-      ibg[i].style.backgroundImage =
-        "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
+    if (ibg[i].querySelector('img')) {
+      ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
     }
   }
 }
 
 ibg();
 
-// Слайдер
+// Слайдеры
+const miniPortfolioSlider = new Swiper('.mini-portfolio__slider', {
+  loop: true,
+  slidesPerView: 1,
 
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+  },
 
+  navigation: {
+    nextEl: '.mini-portfolio__controls-button--right',
+    prevEl: '.mini-portfolio__controls-button--left',
+  },
+});
+const reviewsImgSlider = new Swiper('.reviews-img-slider', {
+  loop: true,
+  slidesPerView: 1,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+
+  navigation: {
+    nextEl: '.reviews__controls-button--right',
+    prevEl: '.reviews__controls-button--left',
+  },
+});
+
+const reviewsTextSlider = new Swiper('.reviews-text-slider', {
+  loop: true,
+  slidesPerView: 1,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+
+  navigation: {
+    nextEl: '.reviews__controls-button--right',
+    prevEl: '.reviews__controls-button--left',
+  },
+});
+
+reviewsImgSlider.controller.control = reviewsTextSlider;
+reviewsTextSlider.controller.control = reviewsImgSlider;
 // window.addEventListener("scroll", () => {
 //   siteHeader.style =
 //     "position: fixed;" + "  top: 0;" + "  left: 0;" + "  right: 0;";
 // });
-
-
